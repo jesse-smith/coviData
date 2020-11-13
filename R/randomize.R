ffa646cab0dc54b6397534e5f0c771466950ce02
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-11-12 23:57:23 -0600

    Clean up randomize functions

--- R/randomize.R
+++ R/randomize.R
@@ -5,8 +5,8 @@
 #' statistical information in the dataset, both univariate and multivariate.
 #' However, since the set of possible output values is the same as the input
 #' values, the minimum and maximum of numeric columns will be the same, as will
-#' the unique values of all columns if `n` is larger than the number of
-#' observations.
+#' the unique values of all columns (if `n` is larger than the number of
+#' observations).
 #'
 #' `randomize()` can perform up- and down-sampling of the input data.
 #' Downsampling is simple random sampling without replacement. Upsampling
@@ -17,13 +17,14 @@
 #'
 #' A stratified version that restricts randomization to occur *within* strata
 #' can be obtained by grouping the data using
-#' \code{\link[dplyr:group_by]{group_by}} prior to calling `randomize()`. In
+#' \code{\link[dplyr:group_by]{group_by()}} prior to calling `randomize()`. In
 #' this case, the relative proportions of the groups within the dataset remain
 #' the same; this allows the user to retain portions of the data's structure
 #' while destroying the remaining information.
 #'
-#' Note that the above is not an implementation of differential privacy and
-#' that, when groups are defined, information both *within and between* grouping
+#' Note that the above only provides anonymity when the number of unique values for
+#' quasi-identifiers (within each group) is large and unique idenfiers are handled separately.
+#' Also note that when groups are defined, information both *within and between* grouping
 #' variables *will not be modified*.
 #'
 #' @param .data A data frame or data frame extension (e.g. a
@@ -48,14 +49,15 @@ randomize <- function(.data, n = NULL, .groups = NULL) {
     )
   )
 
-  n_data <- NROW(.data)
+  n_data <- vctrs::vec_size(.data)
 
   if (rlang::is_empty(n)) {
     n <- n_data
   }
+  
   assertthat::assert_that(
-    n >= 0,
-    msg = "`n` must be greater than or equal to 0"
+    n[[1]] >= 0 & rlang::is_scalar_integerish(n),
+    msg = "`n` must be a non-negative scalar integer"
   )
 
   .data %>%
@@ -91,13 +93,18 @@ randomize <- function(.data, n = NULL, .groups = NULL) {
 #' @return The shuffled input and additional observations
 #'
 #' @export
-sample_unique <- function(x, n) {
+sample_unique <- function(.x, n) {
 
-  unique_x <- vctrs::vec_unique(x)
-  unique_n <- vctrs::vec_unique_count(x)
+  assertthat::assert_that(
+    n[[1]] >= 0 & rlang::is_scalar_integerish(n),
+    msg = "`n` must be a non-negative scalar integer"
+  )
+  
+  unique_x <- vctrs::vec_unique(.x)
+  unique_n <- vctrs::vec_unique_count(.x)
 
   # The input may be very large, so free up for garbage collection ASAP
-  remove(x)
+  remove(.x)
 
   if (n <= unique_n) {
     vec_sample(unique_x, n = n, replace = FALSE)
@@ -128,18 +135,27 @@ sample_unique <- function(x, n) {
 #'   automatically standardised to sum to 1.
 #'
 #' @export
-vec_sample <- function(x, n, replace = FALSE, weight_by = NULL) {
+vec_sample <- function(.x, n, replace = FALSE, weight_by = NULL) {
 
-  if (!rlang::is_empty(weight_by)) {
+  assertthat::assert_that(
+    n[[1]] >= 0 & rlang::is_scalar_integerish(n),
+    msg = "`n` must be a non-negative scalar integer"
+  )
+  
+  wt_null <- is.null(weight_by)
+  wt_empty <- vctrs::vec_is_empty(weight_by)
+  if (wt_empty & !wt_null) {
+    weight_by <- NULL
+  } else if (!wt_empty) {
     weight_by <- weight_by / sum(weight_by)
   }
 
   i <- sample(
-    1:vctrs::vec_size(x),
+    vctrs::vec_seq_along(.x),
     size = n,
     replace = replace,
     prob = weight_by
   )
 
-  vctrs::vec_slice(x, i)
+  vctrs::vec_slice(.x, i = i)
 }
