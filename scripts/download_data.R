33be1b5c62aeb7543c7f7bc887add13c1e3dfbe4
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-11-15 17:33:17 -0600

    Update file names, complete core SAS program implementation

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -2,9 +2,9 @@ library(coviData)
 
 # Download and process AEL data - this will be available first
 download_ael()
-Sys.sleep(5)
+Sys.sleep(1) # Give R time to release file (1 second)
 replace_ael()
-Sys.sleep(5)
+Sys.sleep(1) # Give R time to release file (1 second)
 
 # Download snapshots from "Data for Regions" REDcap project
 # Includes:
@@ -14,14 +14,15 @@ Sys.sleep(5)
 #   - Antigen labs snapshot
 #
 # The NBS and PCR functions will open Excel for you to save as .xlsx; to do this
-# with any of the functions, set `convert = TRUE`.
+# with any of the `download_*`functions, set `convert = TRUE`.
 download_nbs_snapshot(convert = TRUE)
 Sys.sleep(60) # Give Excel time to open (1 minute)
 download_pcr_snapshot(convert = TRUE)
-Sys.sleep(5) # Give R enough time to delete temporary files (1 second)
+Sys.sleep(5) # Give R enough time to delete temporary files (5 seconds)
 download_serology_snapshot()
-Sys.sleep(5) # Give R enough time to delete temporary files (1 second)
+Sys.sleep(5) # Give R enough time to delete temporary files (5 seconds)
 download_antigen_snapshot()
-Sys.sleep(5)
+Sys.sleep(5) # Give R enough time to delete temporary files (5 seconds)
+
 # Check AEL as last step
 check_ael()
