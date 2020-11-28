2c5edc6f2615791fd892bded743c260ffcfea35e
Jesse Smith <jesse.smith@shelbycountytn.gov>
2020-11-28 10:01:02 -0600

    Update download_data.R

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -1,11 +1,5 @@
 library(coviData)
 
-# Download and process AEL data - this will be available first
-download_ael()
-Sys.sleep(3) # Give R time to release file (3 second)
-replace_ael()
-Sys.sleep(3) # Give R time to release file (3 second)
-
 # Download snapshots from "Data for Regions" REDcap project
 # Includes:
 #   - NBS snapshot
@@ -18,11 +12,11 @@ Sys.sleep(3) # Give R time to release file (3 second)
 download_nbs_snapshot()
 Sys.sleep(3) # Give R enough time to delete temporary files (3 seconds)
 convert_nbs_snapshot()
-Sys.sleep(30) # Give Excel time to open (30 seconds)
+Sys.sleep(3) # Give Excel time to release files (3 seconds)
 download_pcr_snapshot()
 Sys.sleep(3) # Give R enough time to delete temporary files (3 seconds)
 convert_pcr_snapshot()
-Sys.sleep(30) # Give Excel time to open (30 seconds)
+Sys.sleep(3) # Give Excel time to release files (3 seconds)
 download_serology_snapshot()
 Sys.sleep(3) # Give R enough time to delete temporary files (3 seconds)
 download_antigen_snapshot()
