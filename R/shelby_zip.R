758460fd57d140739ad0b5f15585e3217b06f175
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-03 12:08:43 -0500

    Document `shelby_zip`

---
+++ R/shelby_zip.R
@@ -0,0 +1,7 @@
+#' Vector of Shelby County ZIP Codes
+#'
+#' @format A `character` with 40 values
+#'
+#' @source US Census Bureau 2010 ZCTA to County Relationship File
+#'   (\url{https://www.census.gov/geographies/reference-files/2010/geo/relationship-files.html#par_textimage_674173622})
+"shelby_zip"
