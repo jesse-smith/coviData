b77678d94a5de17a4c2c61306957f626cb1e31be
aplaxco <Allison.Plaxco@shelbycountytn.gov>
2021-08-23 09:58:57 -0500

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -236,7 +236,8 @@ add_covid_events <- function(gg_obj, lab_y, ...) {
     add_event("2021-02-20", "Broaden Safety Measures", lab_y = lab_y, ...) %>%
     add_event("2021-03-20", "Lift More Restrictions", lab_y = lab_y, ...) %>%
     add_event("2021-04-17", "Emphasize Vaccination", lab_y = lab_y, ...) %>%
-    add_event("2021-05-15", "Ease Mask Requirements", lab_y = lab_y, ...)
+    add_event("2021-05-15", "Ease Mask Requirements", lab_y = lab_y, ...) %>%
+    add_event("2021-06-20", "Mask Requirements Reinstated", lab_y = lab_y, ...)
 }
 
 #' Add a Marker for Today's Date to a Plot
